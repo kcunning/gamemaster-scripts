1c1004b58f0932c677e7692d54dca9f18a62be68
Katie Cunningham <katie.fulton@gmail.com>
2019-08-26 16:45:12 -0400

    Some leftover stuff from the sewer and moving the vamp advance thing to web

---
+++ general/donjon-dungeons/test-js.js
@@ -0,0 +1,3 @@
+var DonJonDynamicLayer = DonJonDynamicLayer || (function() {
+
+})
---
+++ vampire/advance.js
@@ -0,0 +1,11 @@
+// def mult(xp, n, levels=[1, 2, 3, 4, 5]):
+//     poss = []
+//     for level in levels:
+//         x = xp
+//         if level * n <= xp:
+//             poss.append("{o} -> {lvl} x {n}".format(
+//                 o=level-1,
+//                 lvl=level,
+//                 n=int(xp / level / n)))
+//     return poss
+
