f2af85d0dc2208dcc5baa20c87563d262a73c4d1
kcunning <katie.fulton@gmail.com>
2020-04-24 17:41:22 -0400

    So many feats

--- legend/parse_feats.py
+++ legend/parse_feats.py
@@ -7,28 +7,29 @@ f.close()
 
 # pprint(lines)
 
-feats = {}
-i = 0
-while i < len(lines):
-    # Assume first line is title of the feat
-    ttl = lines[i].strip()
-    print ("Title", ttl)
-    d = ''
-    i += 1
-    while i < len(lines):
-        line = lines[i].strip()
-        if not line:
-            i += 1
-            feats[ttl] = {'description': d}
-            break
-        else:
-            d += line
-            i += 1
+# feats = {}
+# i = 0
+# while i < len(lines):
+#     # Assume first line is title of the feat
+#     ttl = lines[i].strip()
+#     print ("Title", ttl)
+#     d = ''
+#     i += 1
+#     while i < len(lines):
+#         line = lines[i].strip()
+#         if not line:
+#             i += 1
+#             feats[ttl] = {'description': d}
+#             break
+#         else:
+#             d += line
+#             i += 1
 
-with open('data/general_feats.json', 'w') as dumpfile:
-    json.dump(feats, dumpfile, indent=4)
+# with open('data/general_feats.json', 'w') as dumpfile:
+#     json.dump(feats, dumpfile, indent=4)
 
 with open('data/general_feats.json') as f:
     jfeats = json.load(f)
 
-print(jfeats)
+for feat in jfeats:
+    print(feat)
